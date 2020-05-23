8fc7efa5f828ce18e4c628f1508ea672a429cdab
Ebenezer Scrooge <1641367382@qq.com>
2020-05-23 16:42:52 +0800

    Fuck ROI
    Conclusion:
    All model with ROI Pooling cannot perform well, BN and deeper structure could only improve a little bit

--- RCNN.py
+++ RCNN.py
@@ -1,7 +1,8 @@
-import os,cv2
+import os, cv2
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+
 tf.compat.v1.disable_eager_execution()
 import tensorflow.keras as keras
 from tensorflow.keras.layers import Dense
@@ -11,31 +12,35 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import pickle
+
 
 class MyLabelBinarizer(LabelBinarizer):
     def transform(self, y):
         Y = super().transform(y)
         if self.y_type_ == 'binary':
-            return np.hstack((Y, 1-Y))
+            return np.hstack((Y, 1 - Y))
         else:
             return Y
+
     def inverse_transform(self, Y, threshold=None):
         if self.y_type_ == 'binary':
             return super().inverse_transform(Y[:, 0], threshold)
         else:
             return super().inverse_transform(Y, threshold)
 
+
 path = "Images"
 annot = "Airplanes_Annotations"
 
+
 def demo():
-    for e,i in enumerate(os.listdir(annot)):
+    for e, i in enumerate(os.listdir(annot)):
         if e < 10:
-            filename = i.split(".")[0]+".jpg"
-            img = cv2.imread(os.path.join(path,filename))
-            path_instance = os.path.join(annot,i)
+            filename = i.split(".")[0] + ".jpg"
+            img = cv2.imread(os.path.join(path, filename))
+            path_instance = os.path.join(annot, i)
             f = open(path_instance)
             df = pd.read_csv(f)
             f.close()
@@ -46,26 +51,27 @@ def demo():
                 y1 = int(row[1][0].split(" ")[1])
                 x2 = int(row[1][0].split(" ")[2])
                 y2 = int(row[1][0].split(" ")[3])
-                cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,0), 2)
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
             plt.figure()
             plt.imshow(img)
             plt.show()
             break
     cv2.setUseOptimized(True);
     ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
-    im = cv2.imread(os.path.join(path,"42850.jpg"))
+    im = cv2.imread(os.path.join(path, "42850.jpg"))
     ss.setBaseImage(im)
     ss.switchToSelectiveSearchFast()
     rects = ss.process()
     imOut = im.copy()
     for i, rect in (enumerate(rects)):
         x, y, w, h = rect
-    #     print(x,y,w,h)
-    #     imOut = imOut[x:x+w,y:y+h]
-        cv2.rectangle(imOut, (x, y), (x+w, y+h), (0, 255, 0), 1, cv2.LINE_AA)
+        #     print(x,y,w,h)
+        #     imOut = imOut[x:x+w,y:y+h]
+        cv2.rectangle(imOut, (x, y), (x + w, y + h), (0, 255, 0), 1, cv2.LINE_AA)
     # plt.figure()
     plt.imshow(imOut)
     plt.show()
+
 
 def get_iou(bb1, bb2):
     assert bb1['x1'] < bb1['x2']
@@ -86,32 +92,33 @@ def get_iou(bb1, bb2):
     assert iou <= 1.0
     return iou
 
+
 def data_generator():
-    train_images=[]
-    train_labels=[]
+    train_images = []
+    train_labels = []
     ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
-    for e,i in enumerate(os.listdir(annot)):
+    for e, i in enumerate(os.listdir(annot)):
         # 对每一个标记文件（csv）进行操作
         try:
             if i.startswith("airplane"):
-                #只有名称带airplane才是有目标的存在的样本
-                filename = i.split(".")[0]+".jpg"
-                print(e,filename)
-                image = cv2.imread(os.path.join(path,filename))
-                df = pd.read_csv(os.path.join(annot,i))
-                gtvalues=[]
+                # 只有名称带airplane才是有目标的存在的样本
+                filename = i.split(".")[0] + ".jpg"
+                print(e, filename)
+                image = cv2.imread(os.path.join(path, filename))
+                df = pd.read_csv(os.path.join(annot, i))
+                gtvalues = []
                 for row in df.iterrows():
                     x1 = int(row[1][0].split(" ")[0])
                     y1 = int(row[1][0].split(" ")[1])
                     x2 = int(row[1][0].split(" ")[2])
                     y2 = int(row[1][0].split(" ")[3])
-                    gtvalues.append({"x1":x1,"x2":x2,"y1":y1,"y2":y2})
-                    #把标签的坐标数据存入gtvalues
+                    gtvalues.append({"x1": x1, "x2": x2, "y1": y1, "y2": y2})
+                    # 把标签的坐标数据存入gtvalues
 
                 ss.setBaseImage(image)
                 ss.switchToSelectiveSearchFast()
                 ssresults = ss.process()
-                #加载SS ROI提出器
+                # 加载SS ROI提出器
 
                 imout = image.copy()
                 counter = 0
@@ -119,41 +126,41 @@ def data_generator():
                 flag = 0
                 fflag = 0
                 bflag = 0
-                for e,result in enumerate(ssresults):
+                for e, result in enumerate(ssresults):
                     if e < 2000 and flag == 0:
-                        #对SS产生的头2k个结果（坐标）进行处理
+                        # 对SS产生的头2k个结果（坐标）进行处理
                         for gtval in gtvalues:
-                            #对这张图上多个标签坐标进行处理
-                            x,y,w,h = result
-                            iou = get_iou(gtval,{"x1":x,"x2":x+w,"y1":y,"y2":y+h})
-                            #计算候选坐标和这一标签坐标的交并比
+                            # 对这张图上多个标签坐标进行处理
+                            x, y, w, h = result
+                            iou = get_iou(gtval, {"x1": x, "x2": x + w, "y1": y, "y2": y + h})
+                            # 计算候选坐标和这一标签坐标的交并比
                             if counter < 30:
-                                #选择交并比大于阈值的头30个候选坐标
+                                # 选择交并比大于阈值的头30个候选坐标
                                 if iou > 0.70:
-                                    #交并比阈值0.7
-                                    timage = imout[y:y+h,x:x+w]
-                                    resized = cv2.resize(timage, (224,224), interpolation = cv2.INTER_AREA)
+                                    # 交并比阈值0.7
+                                    timage = imout[y:y + h, x:x + w]
+                                    resized = cv2.resize(timage, (224, 224), interpolation=cv2.INTER_AREA)
                                     train_images.append(resized)
                                     train_labels.append(1)
                                     counter += 1
-                            else :
-                                fflag =1
-                            if falsecounter <30:
-                                #IoU低于阈值0.3，前30个坐标作为负样本（背景）
+                            else:
+                                fflag = 1
+                            if falsecounter < 30:
+                                # IoU低于阈值0.3，前30个坐标作为负样本（背景）
                                 if iou < 0.3:
-                                    timage = imout[y:y+h,x:x+w]
-                                    resized = cv2.resize(timage, (224,224), interpolation = cv2.INTER_AREA)
+                                    timage = imout[y:y + h, x:x + w]
+                                    resized = cv2.resize(timage, (224, 224), interpolation=cv2.INTER_AREA)
                                     train_images.append(resized)
                                     train_labels.append(0)
                                     falsecounter += 1
-                            else :
+                            else:
                                 bflag = 1
                         if fflag == 1 and bflag == 1:
-                            #print("inside")
+                            # print("inside")
                             flag = 1
         except Exception as e:
             print(e)
-            print("error in "+filename)
+            print("error in " + filename)
             continue
     TI_PKL = open('train_images.pkl', 'wb')
     TL_PKL = open('train_labels.pkl', 'wb')
@@ -163,7 +170,8 @@ def data_generator():
     TL_PKL.close()
     X_new = np.array(train_images)
     y_new = np.array(train_labels)
-    return X_new,y_new
+    return X_new, y_new
+
 
 def data_loader():
     TI_PKL = open('train_images.pkl', 'rb')
@@ -174,67 +182,68 @@ def data_loader():
     TL_PKL.close()
     X_new = np.array(train_images)
     y_new = np.array(train_labels)
-    return X_new,y_new
+    return X_new, y_new
 
-def train(NewModel=False,GenData=False):
-    if(NewModel):
-        vggmodel = tf.keras.applications.VGG16(weights='imagenet', include_top=True)
-        for layers in (vggmodel.layers)[:15]:
+
+def train(NewModel=False, GenData=False):
+    if NewModel:
+        vgg_model = tf.keras.applications.VGG16(weights='imagenet', include_top=True)
+        for layers in vgg_model.layers[:15]:
             layers.trainable = False
-        X= vggmodel.layers[-2].output
+        X = vgg_model.layers[-2].output
         predictions = Dense(2, activation="softmax")(X)
-        model_final = Model(inputs = vggmodel.input, outputs = predictions)
+        model_final = Model(inputs=vgg_model.input, outputs=predictions)
         opt = Adam(lr=0.0001)
         model_final.compile(
-            loss = keras.losses.CategoricalCrossentropy(),
-            optimizer = opt,
+            loss=keras.losses.CategoricalCrossentropy(),
+            optimizer=opt,
             metrics=["accuracy"]
-            )
+        )
     else:
-        model_final = keras.models.load_model("ieeercnn_vgg16_1.h5")
+        model_final = keras.models.load_model("RCNN.h5")
 
-    if(GenData):
-        X_new, y_new = data_generator()
+    if GenData:
+        x_new, y_new = data_generator()
     else:
-        X_new, y_new = data_loader()
+        x_new, y_new = data_loader()
 
     lenc = MyLabelBinarizer()
-    Y =  lenc.fit_transform(y_new)
-    X_train, X_test , y_train, y_test = train_test_split(X_new,Y,test_size=0.10)
+    y = lenc.fit_transform(y_new)
+    x_train, x_test, y_train, y_test = train_test_split(x_new, y, test_size=0.10)
     trdata = ImageDataGenerator(
         horizontal_flip=True,
         vertical_flip=True,
         rotation_range=90
-        )
+    )
     traindata = trdata.flow(
-        x=X_train, 
+        x=x_train,
         y=y_train
-        )
+    )
     tsdata = ImageDataGenerator(
-        horizontal_flip=True, 
-        vertical_flip=True, 
+        horizontal_flip=True,
+        vertical_flip=True,
         rotation_range=90
-        )
+    )
     testdata = tsdata.flow(
-        x=X_test, 
+        x=x_test,
         y=y_test
-        )
+    )
     checkpoint = ModelCheckpoint(
-        "ieeercnn_vgg16_1.h5", 
-        monitor='val_loss', 
-        verbose=1, 
-        save_best_only=False, 
-        save_weights_only=False, 
-        mode='auto', 
+        "RCNN.h5",
+        monitor='val_loss',
+        verbose=1,
+        save_best_only=False,
+        save_weights_only=False,
+        mode='auto',
         save_freq='epoch'
-        )
+    )
     early = EarlyStopping(
-        monitor='val_loss', 
-        min_delta=0, 
-        patience=100, 
-        verbose=1, 
+        monitor='val_loss',
+        min_delta=0,
+        patience=100,
+        verbose=1,
         mode='auto'
-        )
+    )
     with tf.device('/gpu:0'):
         gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
         print(gpus)
@@ -242,47 +251,49 @@ def train(NewModel=False,GenData=False):
             tf.config.experimental.set_memory_growth(gpu, True)
         hist = model_final.fit_generator(
             callbacks=[checkpoint],
-            validation_data = testdata, 
-            validation_steps = 2,
-            generator = traindata, 
-            steps_per_epoch = 10, 
-            epochs = 1000
-            )
+            validation_data=testdata,
+            validation_steps=2,
+            generator=traindata,
+            steps_per_epoch=10,
+            epochs=1000
+        )
     plt.plot(hist.history['loss'])
     plt.plot(hist.history['val_loss'])
     plt.title("model loss")
     plt.ylabel("Loss")
     plt.xlabel("Epoch")
-    plt.legend(["Loss","Validation Loss"])
+    plt.legend(["Loss", "Validation Loss"])
     plt.show()
     plt.savefig('chart loss.png')
+
 
 def test_model_cl():
     model_final = keras.models.load_model("ieeercnn_vgg16_1.h5")
     X_new, y_new = data_loader()
     lenc = MyLabelBinarizer()
-    Y =  lenc.fit_transform(y_new)
-    X_train, X_test , y_train, y_test = train_test_split(X_new,Y,test_size=0.10)
+    Y = lenc.fit_transform(y_new)
+    X_train, X_test, y_train, y_test = train_test_split(X_new, Y, test_size=0.10)
     for test in X_test:
         im = test
         plt.imshow(im)
         img = np.expand_dims(im, axis=0)
-        out= model_final.predict(img)
-        
+        out = model_final.predict(img)
+
         if out[0][0] > out[0][1]:
-            print(str(out)+" "+"plane")
+            print(str(out) + " " + "plane")
         else:
-            print(str(out)+" "+"not plane")
+            print(str(out) + " " + "not plane")
         plt.show()
+
 
 def test_model_od():
     ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
     model_loaded = keras.models.load_model("ieeercnn_vgg16_1.h5")
-    z=0
-    for e,i in enumerate(os.listdir(path)):
+    z = 0
+    for e, i in enumerate(os.listdir(path)):
         if i.startswith("4"):
             z += 1
-            img = cv2.imread(os.path.join(path,i))
+            img = cv2.imread(os.path.join(path, i))
             imout = img.copy()
 
             # Selective Search will be replaced by ROI proposal
@@ -290,17 +301,18 @@ def test_model_od():
             ss.switchToSelectiveSearchFast()
             ssresults = ss.process()
 
-            for e,result in enumerate(ssresults):
+            for e, result in enumerate(ssresults):
                 if e < 2000:
-                    x,y,w,h = result
-                    timage = imout[y:y+h,x:x+w]
-                    resized = cv2.resize(timage, (224,224), interpolation = cv2.INTER_AREA)
+                    x, y, w, h = result
+                    timage = imout[y:y + h, x:x + w]
+                    resized = cv2.resize(timage, (224, 224), interpolation=cv2.INTER_AREA)
                     img = np.expand_dims(resized, axis=0)
-                    out= model_loaded.predict(img)
+                    out = model_loaded.predict(img)
                     if out[0][0] > 0.65:
-                        cv2.rectangle(imout, (x, y), (x+w, y+h), (0, 255, 0), 1, cv2.LINE_AA)
+                        cv2.rectangle(imout, (x, y), (x + w, y + h), (0, 255, 0), 1, cv2.LINE_AA)
             plt.figure()
             plt.imshow(imout)
             plt.show()
 
-test_model_cl()
+
+train(NewModel=True, GenData=True)
